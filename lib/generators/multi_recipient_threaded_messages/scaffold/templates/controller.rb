d9e308a4fcc9c3791c98358463b0baaacd4aed43
Youssef Chaker <youssef.chaker@gmail.com>
2011-01-11 23:29:22 -0500

    cleaning up controller and updating views

--- lib/generators/multi_recipient_threaded_messages/scaffold/templates/controller.rb
+++ lib/generators/multi_recipient_threaded_messages/scaffold/templates/controller.rb
@@ -5,9 +5,9 @@ class <%= message_plural_camel_case %>Controller < ApplicationController
   
   def index
     if params[:mailbox] == "sent"
-      @<%= message_thread_plural_lower_case %> = <%= message_thread_singular_camel_case %>.sent_by(current_user)
+      @<%= message_thread_plural_lower_case %> = <%= message_thread_singular_camel_case %>.sent_by(current_user).uniq
     else
-      @<%= message_thread_plural_lower_case %> = <%= message_thread_singular_camel_case %>.received_by(current_user)
+      @<%= message_thread_plural_lower_case %> = <%= message_thread_singular_camel_case %>.received_by(current_user).uniq
     end
   end
   
@@ -20,18 +20,34 @@ class <%= message_plural_camel_case %>Controller < ApplicationController
   end
   
   def create
+    @user_input = {}
+    @user_input.merge!(params[:message]) unless params[:message].blank?
+    @user_input.merge!(params[:reply]) unless params[:reply].blank?
+    @recipients = get_message_recipients params
+    @errors = validate_message params
     if params[:reply]
+      unless @errors.blank?
+        flash[:error] = "Message Body can not be blank."
+        redirect_to :action => :show, :id => params[:reply][:thread_id]
+        return
+      end
       @<%= message_thread_singular_lower_case %> = <%= message_thread_singular_camel_case %>.find(params[:reply][:thread_id])
       @<%= message_thread_singular_lower_case %>.reply_to_thread(params[:reply].merge(:sender => current_user))
     else
-      recipients = <%= user_singular_camel_case %>.where('id IN ?', params[:message][:recipients_ids])
-      @<%= message_thread_singular_lower_case %>= <%= message_thread_singular_camel_case %>.create_new_thread(params[:message].merge(:sender => current_user, :recipients => recipients))
+      unless @errors.blank?
+        flash[:error] = "Error sending message! Please check all the fields and try again."
+        render :action => :new
+        return
+      end
+      @<%= message_thread_singular_lower_case %> = <%= message_thread_singular_camel_case %>.create_new_thread(params[:message].merge(:sender => current_user, :recipients => @recipients))
     end
-    if @<%= message_thread_singular_lower_case %>.save
-      flash[:notice] = "Message sent"
+    if !@<%= message_thread_singular_lower_case %>.blank?
+      flash[:notice] = "Message sent."
       redirect_to <%= user_singular_lower_case %>_<%= message_plural_lower_case %>_path(current_user)
     else
+      flash[:error] = "Error sending message! Please check all the fields and try again."
       render :action => :new
+      return
     end
   end
   
@@ -50,5 +66,35 @@ class <%= message_plural_camel_case %>Controller < ApplicationController
       end
       redirect_to <%= user_singular_lower_case %>_<%= message_plural_lower_case %>_path(current_user)
     end
+  end
+
+private
+  def get_message_recipients params
+    recipients = []
+    unless params[:message].blank? || (params[:message][:recipients_ids].blank? && params[:message][:to].blank?)
+      ids =  params[:message][:recipients_ids].to_a.is_a?(Array) ?  params[:message][:recipients_ids].to_a : [ params[:message][:recipients_ids].to_a]
+      ids = ids.map { |each| each.to_i }
+      ids = ids + params[:message][:to].split(',')
+      if ids.length == 1
+        recipients = <%= user_singular_camel_case %>.find(ids)
+      elsif ids.length > 1
+        recipients = <%= user_singular_camel_case %>.where('id IN ?', ids).all
+      end
+    end
+    return recipients
+  end
+
+  def validate_message params
+    @errors = {}
+    body = params[:message].blank? ? params[:reply][:body] : params[:message][:body]
+    if body.blank?
+      @errors[:body] = "Message Body can not be blank."
+    end
+    unless params[:reply]
+      if @recipients.blank?
+        @errors[:to] = "Must add recipients to message."
+      end
+    end
+    @errors
   end
 end
--- lib/generators/multi_recipient_threaded_messages/scaffold/templates/helper.rb
+++ lib/generators/multi_recipient_threaded_messages/scaffold/templates/helper.rb
@@ -15,4 +15,19 @@ module <%= message_plural_camel_case %>Helper
     end
     raw(display)
   end
+  
+  def message_input_field name, type, input, errors
+    display = ""
+    value = input.blank? ? "" : input[name] || input[name.to_sym]
+    css_class = (!errors.blank? && !errors[name.to_sym].blank?) ? 'formError' : ''
+    if type == 'text_field'
+      display << text_field_tag("message[#{name}]", value, :class => css_class)
+    elsif type == 'text_area'
+      display << text_area_tag("message[#{name}]", value, :class => css_class)
+    end
+    if (!errors.blank? && !errors[name.to_sym].blank?)
+      display << "<div class='formErrorMessage'>#{errors[name.to_sym]}</div>"
+    end
+    raw(display)
+  end
 end
